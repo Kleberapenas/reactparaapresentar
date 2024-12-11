ba2b32f043cc1e6bb38e44cea17e0c554457c698
Kleber <limadasilvakleber@gmal.com>
2024-12-11 18:45:55 -0300

    update do botão

--- src/App.js
+++ src/App.js
@@ -2,24 +2,19 @@ import React, { useState } from "react";
 import "./App.css";
 
 const App = () => {
-    //  Counter is a state initialized to 0
     const [counter, setCounter] = useState(0);
 
-    // Function is called everytime increment button is clicked
     const handleClick1 = () => {
-        // Counter state is incremented
         setCounter(counter + 1);
     };
 
-    // Function is called everytime decrement button is clicked
     const handleClick2 = () => {
-        // Counter state is decremented
         setCounter(counter - 1);
     };
 
     const handleClick3 = () => {
       setCounter(0)
-    }
+    };
 
     return (
         <div
